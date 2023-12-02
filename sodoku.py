abc74a4720c271b971cda1e669c3a9861ba60115
NicolasSlenko <101363647+NicolasSlenko@users.noreply.github.com>
2023-12-02 16:23:55 -0500

    Update sodoku.py

--- sodoku.py
+++ sodoku.py
@@ -2,6 +2,13 @@ import pygame, sys, sudoku_generator
 
 import cell
 from cell import Cell
+
+
+def print_board(board,row_length,col_length):
+  for row in range(row_length):
+    for col in range(row_length):
+      print(f"{board[row][col]} ", end="")
+    print()
 def start_menu(screen):
     while True:
         for event in pygame.event.get():
@@ -67,8 +74,6 @@ def generate_board(screen, user, cell_list):
 
         font = pygame.font.Font(None, 40)
 
-
-
         #Creates the border
         pygame.draw.line(screen, (0,0,0), (0,0), (600,0), width=10)
         pygame.draw.line(screen, (0, 0, 0), (0, 0), (0, 600), width=10)
@@ -99,29 +104,37 @@ def generate_board(screen, user, cell_list):
 
         #Fill the board with numbers depending on difficulty
 
+        board_size = 9
+        cell_size = 600 // board_size
+
         for x in cell_list:
-            text = font.render(str(x.get_value()), 0, 'black')
-            text_rect = text.get_rect(center=(( (x.get_col() + 1) * 66 ) / 2, ((x.get_row() + 1) * 66) / 2))
-            screen.blit(text, text_rect)
+            value = x.get_value()
+            row, col = x.get_row(), x.get_col()
 
-
-
-
+            text = font.render(str(value), 0, 'black')
+            text_rect = text.get_rect(center=((col + 0.5) * cell_size, (row + 0.5) * cell_size))
+            if value != 0:
+                screen.blit(text, text_rect)
 
         return screen
 
 def play_board(screen, cell_list):
+    oldx = 0
+    oldy = 0
+    input_active = False
+    input_text = ""
+    selected_cell = None  # Variable to store the selected cell
+
+    def check_if_done(cell_list):
+        for cell in cell_list:
+            if cell.value == 0:
+                return False
+        return True
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if reset_rectangle.collidepoint(event.pos):
-                    return 4
-                elif restart_rectangle.collidepoint(event.pos):
-                    return 5
-                elif exit_rectangle.collidepoint(event.pos):
-                    sys.exit()
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
@@ -129,17 +142,59 @@ def play_board(screen, cell_list):
                 y = pos[1] // 66
                 x1 = x
                 y1 = y
-                #cell.unhighlight(screen, y1, x1)
+                cell.unhighlight(screen, oldx, oldy)
                 for z in cell_list:
                     if z.get_row() == y and z.get_col() == x:
-                        #z.highlight()
+                        z.highlight()
+                        oldx = y
+                        oldy = x
+                        selected_cell = z  # Store the selected cell
+                        input_active = True
                         break
+                        # Check if the mouse click is within the exit button's rectangle
+                if exit_rectangle.collidepoint(pos):
+                    sys.exit()  # Exit the program
+
+                if reset_rectangle.collidepoint(pos):
+                    pass
 
 
+            elif event.type == pygame.KEYDOWN:
+                if input_active:
+                    if event.key == pygame.K_RETURN:
+                        # Handle Enter key to store the inputted number
+                        try:
+                            userInput = int(input_text)
+                            print(userInput)
+                            if 1 <= userInput <= 9:  # Check if the input is a valid number
+                                if selected_cell.value == 0:
+                                    selected_cell.displayfill(screen, button_font, userInput)
+                                    if (check_if_done(cell_list)):
+                                        # Initialize an empty 9x9 array
+                                        final_board = [[0 for _ in range(9)] for _ in range(9)]
 
-
-
-
+                                        # Fill in the final_board using the values from the cell_list
+                                        for i in range(9):
+                                            for j in range(9):
+                                                final_board[i][j] = cell_list[i * 9 + j].value
+                                        print_board(final_board,9,9)
+                                        print("")
+                                        print_board(solved_board,9,9)
+                                        if final_board == solved_board:
+                                            print("You Win!")
+                                        else:
+                                            print("You Lose!")
+                                else:
+                                    print("Cannot fill in already filled cell!")
+                            else:
+                                print("Invalid input. Please enter a number between 1 and 9.")
+                        except ValueError:
+                            print("Invalid input. Please enter a valid number.")
+                        input_text = ""  # Reset the input_text after storing the input
+                        input_active = False  # Deactivate input mode
+                    else:
+                        # Handle other key presses to append characters to the input
+                        input_text += event.unicode
 
 
         # Font for the buttons
@@ -180,20 +235,21 @@ if __name__ == '__main__':
 
     board_screen = pygame.display.set_mode([600, 700])
 
-    if user == 1:
-        game_board = sudoku_generator.generate_sudoku(9, 30)
+    if user == 1: #change after to make removed: 30
+        game_board, solved_board = sudoku_generator.generate_sudoku(9, 1)
+        print_board(solved_board,9,9)
         for i in range(0,9):
             for j in range(0,9):
                 c = Cell(game_board[i][j], i, j, board_screen)
                 cell_list.append(c)
     elif user == 2:
-        game_board = sudoku_generator.generate_sudoku(9,40)
+        game_board, solved_board  = sudoku_generator.generate_sudoku(9,40)
         for i in range(0,9):
             for j in range(0,9):
                 c = Cell(game_board[i][j], i, j, board_screen)
                 cell_list.append(c)
     elif user == 3:
-        game_board = sudoku_generator.generate_sudoku(9, 50)
+        game_board, solved_board = sudoku_generator.generate_sudoku(9, 50)
         for i in range(0,9):
             for j in range(0,9):
                 c = Cell(game_board[i][j], i, j, board_screen)
@@ -202,6 +258,7 @@ if __name__ == '__main__':
 
     board_screen = generate_board(board_screen, user, cell_list)
     user = play_board(board_screen, cell_list)
+
 
 
 
@@ -217,7 +274,8 @@ if __name__ == '__main__':
             board_screen = pygame.display.set_mode([600, 700])
             board_screen = generate_board(board_screen, user)
             user = play_board(board_screen)
-
+        if user == 6:
+            sys.exit()
 
 
 
